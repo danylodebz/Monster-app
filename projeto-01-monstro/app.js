d1dcd800c0f21beb84e7f4bdaaed2d1de31efa8f
Danylo <nunesdanylo16@gmail.com>
2024-01-30 14:22:09 -0300

    implementing heal and log

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,65 +1,63 @@
 new Vue({
-    el: '#app',
-    data: {
-      painel: true,
-      listPoints: ['o jogador perdeu 10', 'o monstro perdeu 6'],
-      lifeWidthPlayer: '100%',
-      lifeWidthMonster: '100%',
-      lifePlayer: 100,
-      lifeMonster: 100,
-      attackHistoryPlayer: [],
-      attackHistoryMonster: [],
+  el: '#app',
+  data: {
+    painel: true,
+    lifePlayer: 100,
+    lifeMonster: 100,
+    logs: [],
+  },
+  methods: {
+    randomAtackPlayer() {
+      const value = this.getRandomNumber(6, 11);
+      this.lifePlayer -= value;
+      this.registerLog(`Jogador atacou Monstro com ${value}`, `player`)
     },
-    methods: {
-      randomAtackPlayer() {
-        var randomNumberPlayer = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
-        this.lifeWidthPlayer = `calc(${this.lifeWidthPlayer} - ${randomNumberPlayer}%)`;
-        this.lifePlayer = this.lifePlayer - randomNumberPlayer;
-        this.attackHistoryPlayer.push(randomNumberPlayer);
-        
-      },
-      randomAtackMonster() {
-        var randomNumberMonster = Math.floor(Math.random() * (8 - 4 + 1)) + 4;
-        this.lifeWidthMonster = `calc(${this.lifeWidthMonster} - ${randomNumberMonster}%)`;
-        this.lifeMonster = this.lifeMonster - randomNumberMonster;
-        this.attackHistoryMonster.push(randomNumberMonster);
-      },
-      especialAttackPlayer(){
-        var especialAttackNumber = Math.floor(Math.random() * (9 - 5 + 1) + 5 );
-        this.lifeWidthPlayer = `calc(${this.lifeWidthPlayer} - ${especialAttackNumber}%)`;
-        this.lifePlayer = this.lifePlayer - especialAttackNumber;
-        this.attackHistoryPlayer.push(especialAttackNumber);
-      },
-      especialAttackMonster(){
-        var especialAttackNumber = Math.floor(Math.random() * (11 - 6 + 1) + 6 );
-        this.lifeWidthMonster = `calc(${this.lifeWidthMonster} - ${especialAttackNumber}%)`;
-        this.lifeMonster = this.lifeMonster - especialAttackNumber;
-        this.attackHistoryPlayer.push(especialAttackNumber);
-      },
-      callFunctions() {
-        this.randomAtackMonster();
-        this.randomAtackPlayer();
-      },
-      callEspecialAttacks(){
-        this.especialAttackPlayer();
-        this.especialAttackMonster();
-      },
-      giveUp() {
-        this.painel = !this.painel;
-        this.lifeWidthMonster = '100%';
-        this.lifeWidthPlayer = '100%';
-        this.attackHistoryMonster = [];
-        this.attackHistoryPlayer = [];
-        this.lifeMonster = 100;
-        this.lifePlayer = 100;
-      },
+    randomAtackMonster() {
+      const value = this.getRandomNumber(5, 10);
+      this.lifeMonster -= value;
+      this.registerLog(`Monstro atacou Jogador com ${value}`, `monster`)
     },
-    computed: {
-      combinedList() {
-        return this.attackHistoryPlayer.map((player, index) => {
-          return { player, monster: this.attackHistoryMonster[index] };
-        });
-      },
+    especialAttackPlayer() {
+      const value = this.getRandomNumber(7, 12);
+      this.lifePlayer -= value;
+      this.registerLog(`Jogador atacou Monstro com ${value}`, `player`)
     },
-  });
+    especialAttackMonster() {
+      const value = this.getRandomNumber(5, 10);
+      this.lifeMonster -= value;
+      this.registerLog(`Monstro atacou Jogador com ${value}`, `monster`)
+    },
+    healing() {
+      const value = this.getRandomNumber(4,8);
+      this.lifePlayer += value;
+      const valueAttack = this.getRandomNumber(6,10);
+      this.lifePlayer -= valueAttack
+      this.registerLog(`Jogador ganhou força de ${value}`, `player`)
+    },
+    getRandomNumber(min, max) {
+      const value = Math.random() * (max - min) + min;
+      return Math.round(value);
+    },
+    healEffect() {
+      this.healing();
+      this.randomAtackPlayer();
+    },
+    callFunctions() {
+      this.randomAtackMonster();
+      this.randomAtackPlayer();
+    },
+    callEspecialAttacks() {
+      this.especialAttackPlayer();
+      this.especialAttackMonster();
+    },
+    giveUp() {
+      this.painel = !this.painel;
+      this.lifeMonster = 100;
+      this.lifePlayer = 100;
+    },
+    registerLog(text, cls){
+      this.logs.unshift({text, cls})
+    }
+  },
   
+});
